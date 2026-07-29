a78183ef97d1da794b9bd26947b4064cd442f051
Srishti Priya <if.srishti@gmail.com>
2026-07-30 01:41:15 +0530

    Fix AnalyzeController CORS

--- backend/src/main/java/com/samvid/controller/AnalyzeController.java
+++ backend/src/main/java/com/samvid/controller/AnalyzeController.java
@@ -17,7 +17,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 )
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
 public class AnalyzeController {
 
     private final AnalyzeService analyzeService;
